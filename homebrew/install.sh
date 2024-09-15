20ed6776775237da4c57ce08df8cf06ec0b7463c
himura-shitara <mitarashidango0927@gmail.com>
2024-09-16 00:13:11 +0900

    fix: add brew to path

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,5 +9,6 @@ if test ! $(which brew); then
 
   if test "$(uname)" = "Darwin"; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 fi
