41e400be2d2830c404327da9c4df932e0d2ec15c
himura-shitara <113117363+himura-shitara@users.noreply.github.com>
2024-10-07 16:41:52 +0900

    ' の代わりに " が使われている箇所を修正 (#21)

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,9 +5,9 @@
 set -e
 
 if test ! $(which brew); then
-  echo "  Installing Homebrew..."
+  echo '  Installing Homebrew...'
 
-  if test "$(uname)" = "Darwin"; then
+  if test "$(uname)" = 'Darwin'; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 fi
