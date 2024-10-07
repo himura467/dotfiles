6b7862d335d9f9a3eae3271ffcf63151fa0498d1
himura-shitara <mitarashidango0927@gmail.com>
2024-10-07 16:12:33 +0900

    feat: poetry

---
+++ poetry/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Install Poetry.
+
+set -e
+
+if test ! $(which poetry); then
+  echo "  Installing Poetry..."
+
+  if test ! $(which python); then
+    curl -sSL https://install.python-poetry.org | python -
+  else
+    echo "  Error: Python not found. Python is required to install Poetry."
+  fi
+fi
