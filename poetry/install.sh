19e1cd39032599b6e774dbc1348048e971235ee4
himura-shitara <mitarashidango0927@gmail.com>
2024-10-23 21:24:35 +0900

    fix: reversed python path check

--- poetry/install.sh
+++ poetry/install.sh
@@ -7,7 +7,7 @@ set -e
 if test ! $(which poetry); then
   echo '  Installing Poetry...'
 
-  if test ! $(which python); then
+  if test $(which python); then
     curl -sSL https://install.python-poetry.org | python -
   else
     echo '  Error: Python not found. Python is required to install Poetry.'
