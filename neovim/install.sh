5f797064d60756af660d45bf0b5c785e87f56b52
himura-shitara <mitarashidango0927@gmail.com>
2024-11-12 14:05:16 +0900

    feat: neovim

---
+++ neovim/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Install Neovim.
+
+set -e
+
+if ! command -v nvim > /dev/null; then
+  echo '  Installing Neovim...'
+
+  brew install neovim
+
+  # Install dependencies.
+  brew install ripgrep
+
+  git clone https://github.com/dam9000/kickstart-modular.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+fi
