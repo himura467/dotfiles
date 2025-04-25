a5f3a5d316cbc45ddbc9f34c15f7fb4a1e988c93
himura467 <mitarashidango0927@gmail.com>
2025-04-26 02:06:45 +0900

    refactor: symlink.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ set -e
 DOTFILES_ROOT=$(pwd)
 
 source "$DOTFILES_ROOT/lib/logger.sh"
+source "$DOTFILES_ROOT/lib/symlink.sh"
 
 setup_gitconfig () {
   if ! [[ -f git/gitconfig.local.symlink ]]; then
@@ -34,68 +35,6 @@ setup_gitconfig () {
     sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
 
     success 'gitconfig set up'
-  fi
-}
-
-
-link_file () {
-  local src=$1 dst=$2
-
-  local overwrite= backup= skip=
-  local action=
-
-  if [[ -f "$dst" || -d "$dst" || -L "$dst" ]]; then
-    if [[ "$overwrite_all" == 'false' && "$backup_all" == 'false' && "$skip_all" == 'false' ]]; then
-      local current_src="$(readlink $dst)"
-
-      if [[ "$current_src" == "$src" ]]; then
-        skip=true;
-      else
-        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
-        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
-        read -p '>' -n 1 action
-
-        case "$action" in
-          o )
-            overwrite=true;;
-          O )
-            overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
-          s )
-            skip=true;;
-          S )
-            skip_all=true;;
-          * )
-            ;;
-        esac
-      fi
-    fi
-
-    overwrite=${overwrite:-$overwrite_all}
-    backup=${backup:-$backup_all}
-    skip=${skip:-$skip_all}
-
-    if [[ "$overwrite" == 'true' ]]; then
-      rm -rf "$dst"
-      success "Removed $dst"
-    fi
-
-    if [[ "$backup" == 'true' ]]; then
-      mv "$dst" "${dst}.backup"
-      success "Moved $dst to ${dst}.backup"
-    fi
-
-    if [[ "$skip" == 'true' ]]; then
-      success "Skipped $src"
-    fi
-  fi
-
-  if [[ "$skip" != 'true' ]]; then
-    ln -s "$1" "$2"
-    success "Linked $1 to $2"
   fi
 }
 
