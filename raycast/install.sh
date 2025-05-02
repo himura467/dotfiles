53a8097a79dc70d91c14577ae2211e955b6ea5e1
himura467 <113117363+himura467@users.noreply.github.com>
2025-05-03 02:11:58 +0900

    Raycast を導入 (#50)

---
+++ raycast/install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Install Raycast.
+
+set -e
+
+DOTFILES_ROOT=$(cd $(dirname $0)/..; pwd)
+
+source "$DOTFILES_ROOT/lib/logger.sh"
+
+info 'Installing Raycast'
+
+if command -v brew > /dev/null; then
+  brew install --cask raycast
+
+  success 'Raycast installed'
+else
+  fail 'Homebrew not found. Homebrew is required to install Raycast.'
+fi
