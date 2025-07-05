91d0a31b1b604d5bc7aa01f536a7ef5c85fa6ea7
himura467 <mitarashidango0927@gmail.com>
2025-07-06 03:08:45 +0900

    feat(pyenv): install a specific Python version

--- pyenv/install.sh
+++ pyenv/install.sh
@@ -10,10 +10,44 @@ source "$DOTFILES_ROOT/lib/logger.sh"
 
 info 'Installing pyenv'
 
-if command -v brew > /dev/null; then
-  brew install pyenv
-
-  success 'pyenv installed'
+if ! command -v pyenv > /dev/null; then
+  if command -v brew > /dev/null; then
+    brew install pyenv
+    
+    success 'pyenv installed'
+  else
+    fail 'Homebrew not found. Homebrew is required to install pyenv.'
+  fi
 else
-  fail 'Homebrew not found. Homebrew is required to install pyenv.'
+  success 'pyenv is already installed'
+fi
+
+source "$DOTFILES_ROOT/pyenv/path.zsh"
+
+user 'Would you like to install a specific Python version? (y/n)'
+read -r -p '> ' install_python
+
+if [[ "$install_python" =~ ^[Yy]$ ]]; then
+  available_versions=$(pyenv install --list | grep -E '^[[:space:]]*[0-9]+\.[0-9]+\.[0-9]+$' | sed 's/^[[:space:]]*//' | tail -10)
+  
+  user "Which Python version would you like to install?\n\
+  Available versions (showing latest 10):\n$available_versions"
+  read -r -p '> ' python_version
+  
+  if echo "$available_versions" | grep -q "^$python_version$"; then
+    info "Installing Python $python_version"
+    pyenv install "$python_version"
+    
+    user "Would you like to set Python $python_version as the global default? (y/n)"
+    read -r -p '> ' set_global
+    
+    if [[ "$set_global" =~ ^[Yy]$ ]]; then
+      pyenv global "$python_version"
+      success "Python $python_version set as global default"
+    fi
+    
+    success "Python $python_version installed"
+  else
+    fail 'Invalid Python version. Please choose from the available versions listed above.'
+  fi
 fi
