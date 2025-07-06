01672fd27f869350e12f1a4b50027a703e6174b3
himura467 <mitarashidango0927@gmail.com>
2025-07-06 19:25:02 +0900

    feat(python-dependent): automatically run prerequisite installers when dependencies are missing

--- poetry/install.sh
+++ poetry/install.sh
@@ -11,13 +11,20 @@ source "$DOTFILES_ROOT/lib/logger.sh"
 info 'Installing Poetry'
 
 if ! command -v poetry > /dev/null; then
-  if command -v python > /dev/null; then
-    curl -sSL https://install.python-poetry.org | python -
-
-    success 'Poetry installed'
-  else
-    fail 'Python not found. Python is required to install Poetry.'
+  if ! command -v python > /dev/null; then
+    user 'Python not found. Would you like to install Python first? (y/n)'
+    read -r -p '> ' install_python
+    
+    if [[ "$install_python" =~ ^[Yy]$ ]]; then
+      source "$DOTFILES_ROOT/pyenv/install.sh"
+      source "$DOTFILES_ROOT/pyenv/path.zsh"
+    else
+      fail 'Python is required to install Poetry.'
+    fi
   fi
+  curl -sSL https://install.python-poetry.org | python -
+
+  success 'Poetry installed'
 else
   success 'Poetry is already installed'
 fi
