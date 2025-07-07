52ecae78b2b7177384b468ea9e718ab7dc31a092
himura467 <mitarashidango0927@gmail.com>
2025-07-07 20:18:36 +0900

    fix: correct XDG_CONFIG_HOME path expansion in neovim install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -27,6 +27,6 @@ brew install neovim
 # Install dependencies.
 brew install ripgrep
 
-git clone https://github.com/dam9000/kickstart-modular.nvim.git "$XDG_CONFIG_HOME:-$HOME/.config/nvim"
+git clone https://github.com/dam9000/kickstart-modular.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 
 success 'Neovim installed'
