655ce896f346195c7a9dc29224575fbf603840a5
himura467 <mitarashidango0927@gmail.com>
2025-07-31 03:39:17 +0900

    feat: warn log

--- lib/logger.sh
+++ lib/logger.sh
@@ -3,7 +3,7 @@
 # The logger methods.
 
 info () {
-  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
+  printf "\r  [ \033[00;37m..\033[0m ] %s\n" "$1"
 }
 
 info_list () {
@@ -13,15 +13,19 @@ info_list () {
 }
 
 user () {
-  printf "\r  [ \033[0;33m??\033[0m ] %s\n" "$1"
+  printf "\r  [ \033[0;34m??\033[0m ] %s\n" "$1"
 }
 
 success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+  printf "\r  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+warn () {
+  printf "\r  [\033[0;33mWARN\033[0m] %s\n" "$1"
 }
 
 fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  printf "\r  [\033[0;31mFAIL\033[0m] %s\n" "$1"
   echo ''
   exit
 }
