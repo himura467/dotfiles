eaad9478419e5e6076269b22754c76c4422e03c4
himura467 <akitoshitara@gmail.com>
2026-03-16 08:08:48 +0900

    feat(vite-plus): Node.js version management

--- vite-plus/install.sh
+++ vite-plus/install.sh
@@ -14,3 +14,22 @@ if ! command -v vp > /dev/null; then
 else
   success 'Vite+ is already installed'
 fi
+source "$DOTFILES_ROOT/vite-plus/path.zsh"
+user 'Would you like to install a specific Node.js version? (y/n)'
+read -r -p '> ' install_node
+if [[ "$install_node" =~ ^[Yy]$ ]]; then
+  available_versions=$(vp env list-remote --lts | tail -10)
+  user 'Which Node.js version would you like to install?'
+  info 'Available versions (showing latest 10 LTS):'
+  info_list "$available_versions"
+  read -r -p '> ' node_version
+  info "Installing Node.js $node_version"
+  vp env install "$node_version"
+  user "Would you like to set Node.js $node_version as the global default? (y/n)"
+  read -r -p '> ' set_global
+  if [[ "$set_global" =~ ^[Yy]$ ]]; then
+    vp env default "$node_version"
+    success "Node.js $node_version set as global default"
+  fi
+  success "Node.js $node_version installed"
+fi
