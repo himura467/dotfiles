5b9dff86574263566cad851132c5dc159ab955af
himura467 <akitoshitara@gmail.com>
2026-03-16 07:53:53 +0900

    refactor(vite-plus): skip installation if vp is already on the PATH

--- vite-plus/install.sh
+++ vite-plus/install.sh
@@ -8,5 +8,9 @@ DOTFILES_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")"/..; pwd)
 source "$DOTFILES_ROOT/lib/logger.sh"
 
 info 'Installing Vite+'
-curl -fsSL https://vite.plus | bash
-success 'Vite+ installed'
+if ! command -v vp > /dev/null; then
+  curl -fsSL https://vite.plus | bash
+  success 'Vite+ installed'
+else
+  success 'Vite+ is already installed'
+fi
