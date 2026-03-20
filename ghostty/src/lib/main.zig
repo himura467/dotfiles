fb45e52a2fed95d5cc460f8e80e2b4e89eb3f416
himura467 <akitoshitara@gmail.com>
2026-03-20 19:37:08 +0900

    chore(lib): main.zig

---
+++ ghostty/src/lib/main.zig
@@ -0,0 +1,4 @@
+pub const findDotfilesRoot = @import("paths.zig").findDotfilesRoot;
+pub const getFavorites = @import("favorites.zig").getFavorites;
+pub const getAvailableThemes = @import("themes.zig").getAvailableThemes;
+pub const updateTheme = @import("config.zig").updateTheme;
